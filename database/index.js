63d0d294af1ab8dcb68920a349b026185afff9e9
Kelvy <kelvy0064@gmail.com>
2020-09-02 10:07:15 -0300

    zxc

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const db = require('mongoose')
 
-db.connect('mongodb://localhost/noderest', { useNewUrlParser: true, useUnifiedTopology: true })
+db.connect('mongodb:mongodb+srv://sales-control:<password>@cluster0.ee9wc.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 db.Promise = global.Promise
 
 module.exports = db
